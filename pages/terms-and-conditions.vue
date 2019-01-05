14e19842dac8c77336e0554e2fdd8e290e53dcbf
Chinonso Chukwuogor <ensiocode@gmail.com>
2019-01-05 11:48:16 +0100

    TOC

---
+++ pages/terms-and-conditions.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="container">
+    <div class="heading">
+      <div class="logo">
+        <object data="/logo.svg" type="image/svg+xml"></object>
+      </div>
+      <h1 class="title">Terms and Conditions</h1>
+    </div>
+    <div class="content">
+      <ol>
+        <li>
+          <div class="subtitle">Hello</div>
+          <div class="info">
+            <p>I promise that, except for anything that you gave me to incorporate into the design(s):
+              <br>(a) My work will be original and will not be copied in whole or in part from any other work;
+              <br>(b) I own the rights that I am giving you under this Agreement, or I have secured such rights to any third-party content incorporated into my final design(s); and
+              <br>(c) My work does not violate the patent, copyright, trade secret or other property right of any person, firm or entity.
+            </p>
+            <p>I promise that this Agreement does not conflict with any other contract, agreement or understanding to which I am a party. Finally, I promise that I’ll hold and maintain in strict confidence any confidential information that you provide me (such as proprietary technical or business information), and I will not disclose such information to any third party except as may be required by a court or governmental authority.</p>
+            <p>All design work and the draft showcase will be published on a test server that could be used to view the design on a browser or for review purpose. Before the final payment settlement is done, I in no condition will share any editable source file or any artwork that contains my work and labor.</p>
+            <p>Once the payment is settled and both party agree, every asset, editable source file, document, artwork, programmed files will be shared through a 3rd Party Sharing Service like Dropbox or Google Drive or any storage device.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">Fees and Deposits</div>
+          <div class="info">
+            <p>A 50% deposit of the total fee payable under our proposal is due immediately upon you instructing me to proceed with the website design and development work. The remaining 50% shall become due when the work is completed to your reasonable satisfaction but subject to the terms of the “approval of work” and “rejected work” clauses. I reserve the right not to commence any work until the deposit has been paid in full. The 50% deposit is only refundable if I have not fulfilled my obligations to deliver the work required under the agreement. The deposit is not refundable if the design/development work has been started and you terminate the contract through no fault of mine.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">Supply of Materials</div>
+          <div class="info">
+            <p>You must supply all materials and information required by me to complete the work in accordance with any agreed specification. Such materials may include, but are not limited to, photographs, written copy, logos and other printed material. Where there is any delay in supplying these materials to me which leads to a delay in the completion of work, I have the right to extend any previously agreed deadlines by a reasonable amount. Where you fail to supply materials, and that prevents the progress of the work, I have the right to invoice you for any part or parts of the work already completed.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">Project Variations</div>
+          <div class="info">
+            <p>I am pleased to offer you the opportunity to make revisions to the design up to 6 times. However, in case of requirement for additional design changes more than 5 times or as per mutual agreement, that may arise due to change or modification in original design specification, I may charge an extra fee . The website development phase is flexible and allows certain variations to the original specification. However any major deviation from the specification will attract an extra charge.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">Project Delays and Client Liability</div>
+          <div class="info">
+            <p>Any time frames or estimates that I give are contingent upon your full co-operation and complete and final content in photography for the work pages. During design/development there is a certain amount of feedback required in order to progress to subsequent phases. It is required that a single point of contact be appointed from your side and be made available on a daily basis in order to expedite the feedback process.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">Approval of work</div>
+          <div class="info">
+            <p>On completion of the work you will be notified and have the opportunity to review it. You must notify me in writing of any unsatisfactory points within 3 days of such notification. Any of the work which has not been reported in writing to us as unsatisfactory within the 3-day review period will be deemed to have been approved. Once approved, or deemed approved, work cannot subsequently be rejected and the contract will be deemed to have been completed and the 50% balance of the project price will become due.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">Rejected work</div>
+          <div class="info">
+            <p>If you are not satisfied with the quality of work delivered to you in the first draft, you have full right to cancel the project or modify it. In either case, you can ask for refund of the initial Fifty (50) percent of fee. But no refund will be done if you accept the first draft and want to continue with the initial design.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">Payment</div>
+          <div class="info">
+            <p>Upon completion of the 7-day review period, I will invoice you for the 50% balance of the project.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">Licensing</div>
+          <div class="info">
+            <p>After you pay me in full, you will own rights to the final design(s) that you approve and I create for you as per our agreement . I own all of the concepts/preliminary designs created before you select the final design(s), except for elements in those concepts that are incorporated into the final design(s) that I deliver to you. You will receive rights for the final design(s) only.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">Consequential Loss</div>
+          <div class="info">
+            <p>I will not be liable for any loss or damage which you may suffer which is in any way attributable to any delay in performance or completion of our contract, however that delay arises.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">Subcontracting</div>
+          <div class="info">
+            <p>I reserve the right to subcontract any services that we have agreed to perform for you as we see fit. However it will be notified to you in advance.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">Non Disclosure</div>
+          <div class="info">
+            <p>I (and any subcontractors I engage) agree that I will not at any time disclose any of your confidential information to any third party.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">Backups</div>
+          <div class="info">
+            <p>You are responsible for maintaining your own backups with respect to your website and I will not be liable for restoring any client data or client websites except to the extent that such data loss arises out of a negligent act or Commission by me.</p>
+          </div>
+        </li>
+        <li>
+          <div class="subtitle">I am an independent contractor</div>
+          <div class="info">
+            <p>You agree that I am an independent contractor and not your employee. Although you will provide general direction to me, I will determine, in my sole discretion and expertise the manner and ways in which I will create the design(s) for you. The work that I create for you under our Agreement will not be deemed a “work-for-hire”</p>
+          </div>
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "default"
+};
+</script>
+
+<style lang="scss">
+.container {
+  padding: 10px;
+  .heading {
+    text-align: center;
+    margin-bottom: 40px;
+    .logo {
+      text-align: center;
+      margin-bottom: 30px;
+      object {
+        width: 80px;
+        display: inline-block;
+      }
+    }
+    .title {
+      color: #666;
+      font-weight: 400;
+    }
+  }
+  .content {
+    color: #555;
+    max-width: 1200px;
+    margin: auto;
+    ol {
+      font-weight: bold;
+      font-size: 1.2em;
+      li{
+        margin-bottom: 40px;
+        p{
+          margin-bottom: 20px;
+        }
+      }
+      .subtitle {
+        text-transform: uppercase;
+      }
+      .info {
+        display: block;
+        font-weight: normal;
+        margin: 30px 0 0 -15px;
+      }
+    }
+  }
+}
+</style>
